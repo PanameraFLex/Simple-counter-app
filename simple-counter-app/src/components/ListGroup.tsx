5972d46048f3d25363748b37d55f4085e28b79fe
ChuxInCode <chuxmonye@gmail.com>
2024-05-20 02:55:57 -0400

    update

--- simple-counter-app/src/components/ListGroup.tsx
+++ simple-counter-app/src/components/ListGroup.tsx
@@ -1,6 +1,9 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
   const cities = ["Jo'burg", "Kinshasa", "Lagos", "Kuala Lumpur"];
+  let selectedIndex = -1;
+  let setSelectedIndex = 0
+useState((selectedIndex, setSelectedIndex) => {
   // event Handler
   function handleClick(event: MouseEvent) {
     console.log(event);
@@ -10,7 +13,13 @@ function ListGroup() {
       <h1> PAIN is GAIN</h1>
       <ul className="list-group">
         {cities.map((city, index) => (
-          <li key={city} className="list-group-item" onClick={handleClick}>
+          <li
+            key={city}
+            className={
+              selectedIndex === index ? "list-group-item active" : "list-group"
+            }
+            onClick={handleClick}
+          >
             {city}
           </li>
         ))}
